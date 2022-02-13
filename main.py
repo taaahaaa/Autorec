de3a50626cf31231545985f992a0eda98be75b92
taaahaaa <30396527+taaahaaa@users.noreply.github.com>
2022-02-13 11:27:50 +0330

    Set default to mains

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ current_time = time.time()
 
 parser = argparse.ArgumentParser(description='I-AutoRec ')
 parser.add_argument('--hidden_neuron', type=int, default=500)
-parser.add_argument('--lambda_value', type=float, default=1)
+parser.add_argument('--lambda_value', type=float, default=0.1)
 
-parser.add_argument('--train_epoch', type=int, default=200)
+parser.add_argument('--train_epoch', type=int, default=2000)
 parser.add_argument('--batch_size', type=int,default=100)
 
 parser.add_argument('--optimizer_method', choices=['Adam','RMSProp'],default='Adam')
-parser.add_argument('--grad_clip', type=bool,default=False)
+parser.add_argument('--grad_clip', type=bool,default=True)
 parser.add_argument('--base_lr', type=float, default=1e-3)
 parser.add_argument('--decay_epoch_step', type=int, default=50,help="decay the learning rate for each n epochs")
 

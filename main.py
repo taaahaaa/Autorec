380197d495639b396bebde14d7eabe224eab6052
taaahaaa <30396527+taaahaaa@users.noreply.github.com>
2022-02-13 11:08:26 +0330

    Fix Warning tf

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ parser.add_argument('--random_seed', type=int, default=1000)
 parser.add_argument('--display_step', type=int, default=1)
 
 args = parser.parse_args()
-tf.set_random_seed(args.random_seed)
+tf.compat.v1.set_random_seed(args.random_seed)
 np.random.seed(args.random_seed)
 
 data_name = 'ml-1m'; num_users = 6040; num_items = 3952; num_total_ratings = 1000209; train_ratio = 0.9
@@ -32,9 +32,9 @@ R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R,num_train_ratings,num_t
 user_train_set,item_train_set,user_test_set,item_test_set \
     = read_rating(path, num_users, num_items,num_total_ratings, 1, 0, train_ratio)
 
-config = tf.ConfigProto()
+config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth=True
-with tf.Session(config=config) as sess:
+with tf.compat.v1.Session(config=config) as sess:
     AutoRec = AutoRec(sess,args,
                       num_users,num_items,
                       R, mask_R, C, train_R, train_mask_R, test_R, test_mask_R,num_train_ratings,num_test_ratings,
